3295b6358cc430d63a637137b0bf739ecc08a2ad
golovonogi <kycaiu@gmail.com>
2019-10-21 14:09:08 +0300

    navbar, login, logout, auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ app_name='accounts'
 urlpatterns = [
     path('accounts/register', views.RegisterFormView.as_view(), name="register"),
     path('accounts/login', views.LoginFormView.as_view(), name="login"),
+    path('accounts/settings', views.UserSettingsView.as_view(), name="user_settings"),
+    path('accounts/settings_edit', views.UserSettingsViewEdit.as_view(), name="settings_edit"),
+    path('accounts/logged_out', views.LogoutFormView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
-from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView, UpdateView
 from django.contrib.auth import login as auth_login
+
+from accounts.forms import UserSettings
 
 
 class RegisterFormView(FormView):
@@ -11,12 +15,34 @@ class RegisterFormView(FormView):
     template_name = 'accounts/register.html'
 
     def form_valid(self, form):
-        user=form.save()
+        user = form.save()
         auth_login(self.request, user)
         return super(RegisterFormView, self).form_valid(form)
 
 
 class LoginFormView(LoginView):
-    success_url = '/'
     template_name = 'accounts/login.html'
+
+    def get_redirect_url(self):
+        return '/'
+
+
+class LogoutFormView(LogoutView):
+    next_page = '/'
+
+class UserSettingsView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/settings.html'
+
+class UserSettingsViewEdit(LoginRequiredMixin, UpdateView):
+    form_class = UserSettings
+    success_url = reverse_lazy('accounts:user_settings')
+    template_name = 'accounts/settings_edit.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        form.save()
+        return super(UserSettingsViewEdit, self).form_valid(form)
+
 
